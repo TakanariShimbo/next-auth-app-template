9619effac2d757d22d64684d73f0e485d608d35b
TakanariShimbo <takanari.shimbo@gmail.com>
2024-08-07 17:26:41 +0900

    implement home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@/auth";
 import CustomLink from "@/components/custom-link";
 
 export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-3xl font-bold">🚀NextAuth.js Tutorial</h1>
@@ -15,10 +18,8 @@ export default async function Home() {
         の例を見て、ページを保護してセッションデータを取得する方法を確認してください。
       </div>
       <div className="flex flex-col rounded-md bg-neutral-100">
-        <div className="p-4 font-bold rounded-t-md bg-neutral-200">
-          Current Session
-        </div>
-        <pre className="py-6 px-4 whitespace-pre-wrap break-all"></pre>
+        <div className="p-4 font-bold rounded-t-md bg-neutral-200">Current Session</div>
+        <pre className="py-6 px-4 whitespace-pre-wrap break-all">{JSON.stringify(session, null, 2)}</pre>
       </div>
     </div>
   );
