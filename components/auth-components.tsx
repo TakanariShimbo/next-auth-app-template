45d9fbd625ccbacdc9858ac4ce83bce030b94558
TakanariShimbo <takanari.shimbo@gmail.com>
2024-08-07 17:26:04 +0900

    implement sign in and sing out

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { signIn, signOut } from "@/auth";
 
-export function SignIn({
-  provider,
-  ...props
-}: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
+export function SignIn({ provider, ...props }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form>
+    <form
+      action={async () => {
+        "use server";
+        await signIn(provider);
+      }}
+    >
       <Button {...props}>サインイン</Button>
     </form>
   );
 }
 
-export function SignOut({
-  provider,
-  ...props
-}: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
+export function SignOut({ provider, ...props }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form className="w-full">
+    <form
+      className="w-full"
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
       <Button variant="ghost" className="w-full p-0" {...props}>
         ログアウト
       </Button>
